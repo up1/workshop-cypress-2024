9d2748b88235e56d919bb5dff8b9fe97108017b7
up1 <somkiat.p@gmail.com>
2024-08-04 10:21:01 +0700

    Check first page

--- coffee_cart/cypress/e2e/buy-coffee.cy.js
+++ coffee_cart/cypress/e2e/buy-coffee.cy.js
@@ -1,6 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Buy coffee', () => {
+  it('Buy one Cappuccino', () => {
     cy.visit('https://coffee-cart-steel.vercel.app')
+    cy.get(':nth-child(3) > ul > li').should('have.length', 9)
     cy.get(':nth-child(2) > a').should('have.text', 'cart (0)')
     cy.get('[data-test="checkout"]')
       .should('be.visible')
