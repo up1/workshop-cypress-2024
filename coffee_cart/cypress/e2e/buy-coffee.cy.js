bd3b9da8465c4a2b29f93b9a2a7e43c2a2696049
up1 <somkiat.p@gmail.com>
2024-08-04 13:57:31 +0700

    Working with request interceptor

--- coffee_cart/cypress/e2e/buy-coffee.cy.js
+++ coffee_cart/cypress/e2e/buy-coffee.cy.js
@@ -1,69 +1,94 @@
 /// <reference types="cypress" />
 
 const verifyFirstPage = () => {
-  cy.get('[data-test="coffee-list"] > li').should('have.length', 9)
-    cy.get(':nth-child(2) > a').should('have.text', 'cart (0)')
-    cy.get(':nth-child(2) > a').should('have.attr', 'href', '/cart')
-    cy.get('[data-test="checkout"]')
-      .should('be.visible')
-      .should('have.text', 'Total: $0.00')
-}
+  cy.get('[data-test="coffee-list"] > li').should("have.length", 9);
+  cy.get(":nth-child(2) > a").should("have.text", "cart (0)");
+  cy.get(":nth-child(2) > a").should("have.attr", "href", "/cart");
+  cy.get('[data-test="checkout"]')
+    .should("be.visible")
+    .should("have.text", "Total: $0.00");
+};
 
 const addProductToCart = (name, price) => {
-  cy.get(`[data-cy="${name}"]`).click()
-  cy.get(':nth-child(2) > a').should('have.text', 'cart (1)')
-  cy.get('[data-test="checkout"]')
-    .should('have.text', 'Total: $'+ price)
-}
+  cy.get(`[data-cy="${name}"]`).click();
+  cy.get(":nth-child(2) > a").should("have.text", "cart (1)");
+  cy.get('[data-test="checkout"]').should("have.text", "Total: $" + price);
+};
 
-describe('Buy coffee', () => {
+describe("Buy coffee", () => {
+  before(() => {
+    // Run once before all tests
+    cy.intercept(
+      {
+        method: "POST",
+        url: "https://www.google-analytics.com/**",
+      },
+      { message: "ieieiie" }
+    ).as("GoogleAnalytics");
 
-  it('Get text from element', () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/list.json",
+      },
+      { fixture: "products.json" }
+    ).as("List of products");
+  });
+
+  beforeEach(() => {
+    // Run before each test
+  });
+
+  it("Get text from element", () => {
     // cy.step('open a page')
-    cy.visit('https://coffee-cart-steel.vercel.app')
+    cy.visit("https://coffee-cart-steel.vercel.app");
     // cy.get('[data-cy="Cappuccino"]').click()
 
     // cy.step('Add product to cart')
-    cy.getByDataCy('Cappuccino').click()
+    cy.getByDataCy("Cappuccino").click();
 
     // cy.step('Get data from product')
-    cy.get('[data-cy="Cappuccino"] > div:nth-child(2)').invoke('text').then((text) => {
-      console.log(text)
-    });
-  })
+    cy.get('[data-cy="Cappuccino"] > div:nth-child(2)')
+      .invoke("text")
+      .then((text) => {
+        console.log(text);
+      });
+  });
 
-  it('Mouse over element', () => {
-    cy.visit('https://coffee-cart-steel.vercel.app')
-    cy.get('[data-cy="Cappuccino"]', { timeout: 5000 }).click()
-    cy.get('[data-test="checkout"]').trigger('mouseover')
-  })
+  it("Mouse over element", () => {
+    cy.visit("https://coffee-cart-steel.vercel.app");
+    cy.get('[data-cy="Cappuccino"]', { timeout: 5000 }).click();
+    cy.get('[data-test="checkout"]').trigger("mouseover");
+  });
 
-  it.only('Buy one Cappuccino', () => {
-    cy.step('Open a page')
-    cy.visit('https://coffee-cart-steel.vercel.app')
-    verifyFirstPage()
+  it.only("Buy one Cappuccino", () => {
+    cy.step("Open a page");
+    cy.visit("https://coffee-cart-steel.vercel.app");
+    verifyFirstPage();
 
     // Add Cappuccino to cart
-    cy.step('Add product to cart')
-    cy.addProductToCart('Cappuccino', '19.00')
+    cy.step("Add product to cart");
+    cy.addProductToCart("Cappuccino", "19.00");
 
     // Pay for the coffee
-    cy.step('Pay for the coffee')
-    cy.get('[data-test="checkout"]').click()
-    cy.get('.modal-content').should('be.visible')
-    cy.get('h1').should('have.text', 'Payment details')
+    cy.step("Pay for the coffee");
+    cy.get('[data-test="checkout"]').click();
+    cy.get(".modal-content").should("be.visible");
+    cy.get("h1").should("have.text", "Payment details");
 
     // Fill in payment details
-    cy.step('Fill in payment details')
-    cy.get('#name').type('Somkiat')
-    cy.get('#email').type('somkiat@xxx.com')
+    cy.step("Fill in payment details");
+    cy.get("#name").type("Somkiat");
+    cy.get("#email").type("somkiat@xxx.com");
 
     // Pay with success
-    cy.step('Pay with success')
-    cy.get('#submit-payment').click()
-    cy.get('.snackbar').should('have.text', 'Thanks for your purchase. Please check your email for payment.')
-    cy.get(':nth-child(2) > a').should('have.text', 'cart (0)')
-    cy.get('[data-test="checkout"]')
-      .should('have.text', 'Total: $0.00')
-  })
-})
+    cy.step("Pay with success");
+    cy.get("#submit-payment").click();
+    cy.get(".snackbar").should(
+      "have.text",
+      "Thanks for your purchase. Please check your email for payment."
+    );
+    cy.get(":nth-child(2) > a").should("have.text", "cart (0)");
+    cy.get('[data-test="checkout"]').should("have.text", "Total: $0.00");
+  });
+});
